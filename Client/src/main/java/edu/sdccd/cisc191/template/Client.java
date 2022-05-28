79c3c9fdff7e439b9f258ab69182cff03756f15e
Damnmac <97910904+Damnmac@users.noreply.github.com>
2022-05-28 11:26:25 -0700

    Update Client.java

--- Client/src/main/java/edu/sdccd/cisc191/template/Client.java
+++ Client/src/main/java/edu/sdccd/cisc191/template/Client.java
@@ -1,51 +1,101 @@
-package edu.sdccd.cisc191.template;
+import java.io.*; // 1.0 Java Bbasics
+import java.util.Scanner;
+import java.util.function.DoubleFunction;
 
-import java.net.*;
-import java.io.*;
+public class Main
+{
+    public static void main(String[] args)
+    {
 
-/**
- * This program opens a connection to a computer specified
- * as the first command-line argument.  If no command-line
- * argument is given, it prompts the user for a computer
- * to connect to.  The connection is made to
- * the port specified by LISTENING_PORT.  The program reads one
- * line of text from the connection and then closes the
- * connection.  It displays the text that it read on
- * standard output.  This program is meant to be used with
- * the server program, DateServer, which sends the current
- * date and time on the computer where the server is running.
- */
 
-public class Client {
-    private Socket clientSocket;
-    private PrintWriter out;
-    private BufferedReader in;
 
-    public void startConnection(String ip, int port) throws IOException {
-        clientSocket = new Socket(ip, port);
-        out = new PrintWriter(clientSocket.getOutputStream(), true);
-        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-    }
 
-    public CustomerResponse sendRequest() throws Exception {
-        out.println(CustomerRequest.toJSON(new CustomerRequest(1)));
-        return CustomerResponse.fromJSON(in.readLine());
-    }
-
-    public void stopConnection() throws IOException {
-        in.close();
-        out.close();
-        clientSocket.close();
-    }
-    public static void main(String[] args) {
-        Client client = new Client();
+        Stock one = new Stock("one",19);
+        Stock two = new Stock("two", 200);
+        Stock three = new Stock("three",30);
+        Stock[] list = new Stock[3];
+        list[0] = one;
+        list[1] = two;
+        list[2] = three;
+        LinkedList <String> names = new LinkedList<>();
+        Scanner s = new Scanner(System.in);
+        String name;
+        String age;
+        String occupation;
+        boolean firstTime = true;
+        //7.0 I/O Streams
         try {
-            client.startConnection("127.0.0.1", 4444);
-            System.out.println(client.sendRequest().toString());
-            client.stopConnection();
-        } catch(Exception e) {
+            File CustomerInfo = new File("CustomerInfo.txt");
+            if (CustomerInfo.createNewFile()) {
+                System.out.println("Welcome");
+            } else {
+                firstTime = false;
+                System.out.println("Welcome Back");
+            }
+        } catch (IOException e) {
+            System.out.println("Error");
             e.printStackTrace();
         }
-    }
-} //end class Client
+        String moreToAdd = "y";
+        while((moreToAdd.equals("y")))
+        {
+            System.out.println("Enter Name of Customer");
+            name = s.nextLine();
+            System.out.println("Enter age of Customer");
+            age = s.nextLine();
+            System.out.println("Enter occupation of Customer");
+            occupation= s.nextLine();
+            RegularCustomer p = new RegularCustomer(name,age,occupation);
+            RegularCustomer.addCustomer(p);
+            names.add(names.size(),p.getName());
 
+            System.out.println("more customer?");
+            moreToAdd = s.nextLine();
+        }
+        int i = 0;
+        while(i < RegularCustomer.getCustomerCount())
+        {
+            System.out.println("Buy Stocks for " + RegularCustomer.customers.get(i).name + "?");
+            moreToAdd = s.nextLine();
+            if(moreToAdd.equals("y"))
+            {
+               int j = 0;
+               int num;
+               while(j < list.length)
+               {
+                  System.out.print("How many of " + list[j].getSymbol());
+                  num = s.nextInt();
+                  RegularCustomer.customers.get(i).stocks[j][0] = num;
+                  j++;
+               }
+            }
+            moreToAdd = s.nextLine();
+            i++;
+        }
+
+        System.out.println("\n\n\n\nEnd of Day Report\n\n");
+
+
+
+        for(i = 0; i < RegularCustomer.getCustomerCount(); i++)
+        {
+            System.out.println(RegularCustomer.customers.get(i).getName() + " owns\n");
+            System.out.println(RegularCustomer.customers.get(i).stocks[0][0] + " of " + list[0].getSymbol());
+            System.out.println(RegularCustomer.customers.get(i).stocks[1][0] + " of " + list[1].getSymbol());
+            System.out.println(RegularCustomer.customers.get(i).stocks[2][0]+ " of " + list[2].getSymbol() +"\n\n\n");
+        }
+
+
+
+
+        try{
+            FileWriter Writer = new FileWriter("CustomerInfo.txt");
+            Writer.write("StockAPP Customer Names:\n\n\n");
+            Writer.append(names.traverse());
+            Writer.close();
+        } catch(Exception e) {}
+
+    }
+
+
+}
